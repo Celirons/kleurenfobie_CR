eaca3eabd0c4bb243bdb19b7b1aadb051b3b675c
Céline <celirons@student.arteveldehs.be>
2018-12-21 18:49:38 +0100

    test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".nieuwsbrief").click(function(){
+
+        $(".formwrapper").toggleClass("verborgen");
+        $(this).hide();
+
+    })
+    $(".closebutton").click(function(){
+        $(".formwrapper").toggleClass("verborgen");
+        $(".nieuwsbrief").show();
+
+    })
+});
